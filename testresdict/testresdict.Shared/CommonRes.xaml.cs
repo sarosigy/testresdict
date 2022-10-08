facd82b7a6fc8af7062cc9eff850a3294caddfb2
George Sarosi <sarosigy@hotmail.com>
2022-10-08 13:43:43 +0200

    Initial commit

---
+++ testresdict/testresdict.Shared/CommonRes.xaml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+
+namespace testresdict
+{
+    public sealed partial class CommonRes : ResourceDictionary
+    {
+        public CommonRes()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnAccept_Click(object sender, RoutedEventArgs e)
+        {
+            flAccRej.Hide();
+        }
+
+        private void btnReject_Click(object sender, RoutedEventArgs e)
+        {
+            flAccRej.Hide();
+        }
+    }
+}
